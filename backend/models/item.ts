dc6fb42b87fc3f1bcf7a0d03b9c72bcee999e5bd
bibikov nikita <bibikovnikita777@gmail.com>
2024-08-24 07:43:40 +0600

    defined interfaces

---
+++ backend/models/item.ts
@@ -0,0 +1,9 @@
+export interface Item {
+    id: string;
+    categoryId: string;
+    locationId: string;
+    name: string;
+    description?: string;
+    photo?: string;
+    inventoryDate: string;
+}
